a50b06594ac785d6bb4c810fbb0f689d88145caf
unknown <radhian21@gmail.com>
2019-08-14 10:55:35 +0700

    add db sample

---
+++ postest_radhian.sql
@@ -0,0 +1,38 @@
+/*
+SQLyog Ultimate v11.21 (64 bit)
+MySQL - 10.1.32-MariaDB : Database - postest_radhian
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`postest_radhian` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `postest_radhian`;
+
+/*Table structure for table `events` */
+
+DROP TABLE IF EXISTS `events`;
+
+CREATE TABLE `events` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `place` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+/*Data for the table `events` */
+
+insert  into `events`(`id`,`title`,`date`,`place`) values (3,'Konser Dangdut','2019-08-17','Lapangan Pogung Lor'),(4,'Create Event','2019-08-27','Kantor Gamatechno'),(5,'Create Event','2019-08-27','Kantor Gamatechno'),(6,'Create Event 2','2019-08-27','Kantor Gamatechno 2');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
